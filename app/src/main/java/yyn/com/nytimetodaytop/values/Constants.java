6c16c4cdb8d1b46c56d71771781146686d705618
Yuning Yu <lmzf318@gmail.com>
2019-03-06 23:05:16 -0500

    Update env properties for the whole project and API keys of NY Times

--- app/src/main/java/yyn/com/nytimetodaytop/values/Constants.java
+++ app/src/main/java/yyn/com/nytimetodaytop/values/Constants.java
@@ -4,7 +4,7 @@ package yyn.com.nytimetodaytop.values;
  * Created by jiy on 2017-04-11.
  */
 public class Constants {
-    public final static String NYTOPS_API_KEY = "64057f92e5174dbcb3571158896352c6";
+    public final static String NYTOPS_API_KEY = "nQ1IQngbAXE0oLj4RVi8TElXtkPAepgP";
     public final static String BASE_URL = "https://api.nytimes.com/svc/topstories/v2/";
     public final static String FORMAT = "json";
 
